c69958ea181bd12a0f945367db243b3d2c3363b7
wanganlin <inbox@wanganlin.com>
2023-09-01 10:43:11 +0800

    增加脚本工具

---
+++ scripts/include/open_api.sh
@@ -0,0 +1,22 @@
+Echo_Green '------------------------------'
+Echo_Green ' 生成swagger接口文档'
+Echo_Green '------------------------------'
+
+vendor/bin/openapi app/Http/Auth/ app/Entities/ \
+  -o public/swagger/auth.json -f json
+
+vendor/bin/openapi app/Http/Manager/ app/Entities/ \
+  app/Bundles/Article/Controllers/Manager/ app/Bundles/Article/Requests/ app/Bundles/Article/Responses/ \
+  -o public/swagger/manager.json -f json
+
+vendor/bin/openapi app/Http/User/ app/Entities/ \
+  -o public/swagger/user.json -f json
+
+vendor/bin/openapi app/Http/Web/ app/Entities/ \
+  -o public/swagger/web.json -f json
+
+Echo_Green '------------------------------'
+Echo_Green ' 生成typescript接口'
+Echo_Green '------------------------------'
+
+php artisan gen:interface
